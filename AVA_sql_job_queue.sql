73d79eec68e195d60142d746cee59595a1856fbf
swinner86 <64804078+swinner86@users.noreply.github.com>
2025-02-04 13:00:19 +0100

    Create AVA_sql_job_queue.sql

---
+++ AVA_sql_job_queue.sql
@@ -0,0 +1,23 @@
+SELECT 'dbo.Oberland Energie AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[Oberland Energie AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.AVAG Betriebs AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[AVAG Betriebs AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.Fernwärme Thun AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[Fernwärme Thun AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.AVAG KVA AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[AVAG KVA AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.Future Hub AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[Future Hub AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.Holzkraftwerk Aarberg AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[Holzkraftwerk Aarberg AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.AVAG Umwelt AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[AVAG Umwelt AG$Job Queue Entry]
+UNION ALL
+SELECT 'dbo.SOGES AG' AS DatabaseObject, [Description], [Status], [Earliest Start Date_Time]
+FROM [dbo].[SOGES AG$Job Queue Entry]
